5d2fc68dfc2866f6415803648e707e1590ecf02a
GuidoPisarra <pisarraguido@gmail.com>
2025-10-10 14:02:10 -0300

    recibo primer respuesta del microservicio de pagos, por lo tanto tengo la comunicacion con los servicios con token

--- api-gateway/src/controllers/payment.controller.ts
+++ api-gateway/src/controllers/payment.controller.ts
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/require-await */
+
 import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
@@ -30,10 +28,7 @@ export class PaymentsController {
       { cmd: 'create_payment' },
       { user, data: body },
     );
-
-    // Espera la respuesta del microservicio
     const result = await lastValueFrom(observable);
-
-    return result; // ahora el cliente HTTP recibe la respuesta
+    return result;
   }
 }
